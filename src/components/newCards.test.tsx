beaba6be9766a6caa638d19dce9a7a079319a4be
Mikhail Stankevich <mikhailstn1@gmail.com>
2023-04-11 14:27:51 +0300

    fix: fix test

--- src/components/newCards.test.tsx
+++ src/components/newCards.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import NewCards from './newCards';
 import { describe, expect, it } from 'vitest';
 
